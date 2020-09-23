a8828cdb98143be461fe8c94aa6e206af825253c
Ubuntu <ubuntu@ip-172-31-39-62.eu-west-1.compute.internal>
2020-09-23 10:00:51 +0000

    Database connection complete

--- src/main/java/io/pivotal/pal/tracker/ConfigurationClass.java
+++ src/main/java/io/pivotal/pal/tracker/ConfigurationClass.java
@@ -3,11 +3,13 @@ package io.pivotal.pal.tracker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class ConfigurationClass {
     @Bean
-    public TimeEntryRepository retrieveRepo() {
-        return new InMemoryTimeEntryRepository();
+    public JdbcTimeEntryRepository retrieveRepo(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
 }
